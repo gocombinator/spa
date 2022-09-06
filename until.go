a9702e401b2ca6a446f9ee0956f9f82aa2eb3164
Mirek Rusin <mirek@me.com>
2022-09-06 19:41:18 +0200

    fix: until

--- until.go
+++ until.go
@@ -1,5 +1,9 @@
 package spa
 
+import (
+	"unicode/utf8"
+)
+
 // Until matches until provided parser succeeds or end of input is reached.
 // Doesn't consume input of succeeding parser.
 // Returns string with zero or more length.
@@ -9,9 +13,10 @@ package spa
 // Use [Min] to limit minimum length.
 func Until(p Parser) Parser {
 	return func(in string) (any, int, error) {
-		var o = 0
-		for {
-			if _, w, err := p(in[o:]); err == nil && w > 0 {
+		var n = len(in)
+		for o := 0; o < n; {
+			if _, _, err := p(in[o:]); err != nil {
+				var _, w = utf8.DecodeRuneInString(in[o:])
 				o += w
 			} else {
 				return in[:o], o, nil
